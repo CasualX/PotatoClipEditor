413af0b586a43bf41e4e700eb6bcd0f94c2eca2b
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2021-05-07 19:42:25 +0300

    Fix drag and drop functionality.
    Fix Vite/Tailwind configs after changing root directory to docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
 	mode: 'jit',
-	purge: ['./index.html'],
+	purge: ['./docs/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
